a420abb44cb9e2a258ffe5e7dc05b4589aaa7094
Nandini116200 <gargnandini1106@gmail.com>
2026-03-25 23:27:10 +0530

    Update day2.cpp

--- day2.cpp
+++ day2.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-  cout<<"this is my day 2 of DSA Learning";
+  //cout<<"this is my day 2 of DSA Learning";
+  int age;
+  cout<<"Enter your age: ";
+  cin >> age;
+
+  cout<<"your age is:"<<age<<endl;
+  return 0;
 }
