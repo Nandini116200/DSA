a744bd7bbac896205f6105aef7173ede146c1b07
Nandini116200 <gargnandini1106@gmail.com>
2026-03-26 23:43:52 +0530

    Add If-Else statement to check number sign

--- day3.cpp
+++ day3.cpp
@@ -1,1 +1,18 @@
+//If-Else statement:
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n = 50;
+    cout<<"enter number:\n";
+    cin>>n;
+    if (n >= 0){
+        cout << "n is positive\n";
+
+    }else{
+        cout << "n is negative\n";
+    }
+    
+    return 0;
+
+}
