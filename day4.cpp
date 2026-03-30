734e60ee11e4bdbe8e84acccc52fba665b30bd02
Nandini116200 <gargnandini1106@gmail.com>
2026-03-30 11:52:56 +0530

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -34,4 +34,22 @@ int main(){
         cout<<endl;
     }    
     return 0;
-}                      
+}    
+
+(3) /*to print pattern *
+                   **
+                   ***
+                   ****  */
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for( int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<" * " ;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}             
