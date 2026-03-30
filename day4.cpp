148d8ef423d59e0eaa3a2379ff2c69d43284ef30
Nandini116200 <gargnandini1106@gmail.com>
2026-03-30 12:50:30 +0530

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -90,4 +90,19 @@ int main(){
     return 0;
 }
 
+(6) /* to print 10
+                210
+                3210
+                43210 */
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j>=0; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
 
